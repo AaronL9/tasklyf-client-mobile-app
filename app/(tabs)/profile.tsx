b68facacc9b0039698356b427f590f8e84cec48b
Aaron <aaron.lomibao09@gmail.com>
2024-10-14 20:37:44 +0800

    Profile Header Added

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,11 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import ProfileHeader from "@/components/Profile/ProfileHeader";
 
 export default function Profile() {
   return (
-    <View>
-      <Text>profile</Text>
+    <View style={{ alignItems: "center" }}>
+      <ProfileHeader />
     </View>
   );
 }
